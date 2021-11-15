d575977861e1bd5935a4568af7789e7200cf57f3
kefaming <kefaming@163.com>
2021-11-15 22:43:51 +0800

    版本回退至6d8cf19249e89d23df91d7de3e536f4895c79135

--- cloud-admin/src/test/java/com/cloud/user/CloudUserApplicationTests.java
+++ cloud-admin/src/test/java/com/cloud/user/CloudUserApplicationTests.java
@@ -1,26 +1,26 @@
-package com.cloud.user;
-
-import com.cloud.admin.entity.User;
-import com.cloud.admin.mapper.UserMapper;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import javax.annotation.Resource;
-import java.util.List;
-
-@SpringBootTest
-class CloudUserApplicationTests {
-
-    @Resource
-    private UserMapper userMapper;
-
-    @Test
-    public void testSelect() {
-        System.out.println(("----- selectAll method test ------"));
-        List<User> userList = userMapper.selectList(null);
-        Assertions.assertEquals(5, userList.size());
-        userList.forEach(System.out::println);
-    }
-
-}
+//package com.cloud.user;
+//
+//import com.cloud.admin.entity.User;
+//import com.cloud.admin.mapper.UserMapper;
+//import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//import javax.annotation.Resource;
+//import java.util.List;
+//
+//@SpringBootTest
+//class CloudUserApplicationTests {
+//
+//    @Resource
+//    private UserMapper userMapper;
+//
+//    @Test
+//    public void testSelect() {
+//        System.out.println(("----- selectAll method test ------"));
+//        List<User> userList = userMapper.selectList(null);
+//        Assertions.assertEquals(5, userList.size());
+//        userList.forEach(System.out::println);
+//    }
+//
+//}
